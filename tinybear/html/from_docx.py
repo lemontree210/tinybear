8585428cc31432ac74ca7d148cdd6992f5e1b78c
Dmitry Kolomatskiy <58207913+lemontree210@users.noreply.github.com>
2025-08-07 16:27:03 +0300

    rename/move/refactor

--- tinybear/html/from_docx.py
+++ tinybear/html/from_docx.py
@@ -19,18 +19,17 @@ r[style-name='Code'] => code
 """
 
 
-def convert_one_to_html(
+def convert_file_from_doc(
     path_to_file: Path, output_dir: Path = DEFAULT_OUTPUT_DIR, print_html: bool = True
 ) -> Path:
+    """Read from DOC(x) file, write to HTML file, return its path."""
 
-    with path_to_file.open(mode="rb") as docx_file:
-        result = mammoth.convert_to_html(docx_file, style_map=STYLE_MAP)
-        html = result.value
-
-    output_path = output_dir / f"{path_to_file.stem}.html"
+    html = read_from_doc(path_to_file=path_to_file)
 
     if print_html:
         logging.info(html)
+
+    output_path = output_dir / f"{path_to_file.stem}.html"
 
     with output_path.open(mode="w+", encoding="utf-8") as output_file:
         output_file.write(html)
@@ -38,15 +37,26 @@ def convert_one_to_html(
     return output_path
 
 
-def convert_all(
+def convert_all_docs(
     input_dir: Path = DEFAULT_INPUT_DIR,
     output_dir: Path = DEFAULT_OUTPUT_DIR,
     print_html: bool = True,
 ) -> None:
+    """Converts all .DOC(x) files in a directory."""
     for file in input_dir.glob("*.doc*"):
         logging.info(f"Converting {file.name}")
-        convert_one_to_html(path_to_file=file, output_dir=output_dir, print_html=print_html)
+        convert_file_from_doc(path_to_file=file, output_dir=output_dir, print_html=print_html)
+
+
+def read_from_doc(
+    path_to_file: Path,
+    style_map: str = STYLE_MAP,
+) -> str:
+    """Read binary content from doc file and produce string HTML."""
+    with path_to_file.open(mode="rb") as docx_file:
+        result = mammoth.convert_to_html(docx_file, style_map=style_map)
+        return result.value
 
 
 if __name__ == "__main__":
-    convert_all()
+    convert_all_docs()
